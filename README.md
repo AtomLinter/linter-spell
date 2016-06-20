1ce2a69d6576f348e012e1c882b44e72256a55a7
Tarn Burton <twburton@gmail.com>
2016-06-20 15:57:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# linter-hunspell
+Atom Linter plugin for hunspell
+
+Nothing works yet!
---
+++ spec/linter-hunspell-spec.js
@@ -0,0 +1,25 @@
+"use babel";
+
+import * as path from 'path';
+
+describe('The hunspell provider for Atom Linter', () => {
+  const lint = require('../lib/main').provideLinter().lint
+
+  beforeEach(() => {
+    waitsForPromise(() => {
+      return atom.packages.activatePackage("linter-hunspell")
+    })
+  })
+
+  it('finds a spelling in "foo.c"', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(path.join(__dirname, 'files', 'foo.tex')).then(editor => {
+        return lint(editor).then(messages => {
+          expect(messages.length).toEqual(1)
+          expect(messages[0].type).toEqual("Warning")
+          expect(messages[0].text).toEqual("ghj")
+        })
+      })
+    })
+  })
+})
