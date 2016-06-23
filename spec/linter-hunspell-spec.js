7610e1076c0df1983157f8041cd3bf068c21298d
Tarn Burton <twburton@gmail.com>
2016-06-23 07:17:55 -0400

    Working on spec

--- spec/linter-hunspell-spec.js
+++ spec/linter-hunspell-spec.js
@@ -15,6 +15,7 @@ describe('The hunspell provider for Atom Linter', () => {
     waitsForPromise(() => {
       return atom.workspace.open(path.join(__dirname, 'files', 'foo.tex')).then(editor => {
         return lint(editor).then(messages => {
+          console.log(messages)
           expect(messages.length).toEqual(1)
           expect(messages[0].type).toEqual('Warning')
           expect(messages[0].text).toEqual('ghj')
