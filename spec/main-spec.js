4920ec51c81f32dcd98e00034a7cb7bafac12998
Tarn Burton <twburton@gmail.com>
2016-08-27 19:39:16 -0400

    Misc cleanup

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -4,25 +4,19 @@ import * as _ from 'lodash'
 import * as path from 'path'
 
 describe('The Ispell provider for Atom Linter', () => {
-  atom.config.set('linter-spell.spellPath', 'hunspell')
-  atom.config.set('linter-spell.defaultLanguages', [])
-  const main = require('../lib/main')
-  // main.activate()
-  // main.consumeDictionary(main.provideDictionary())
   const lint = require('../lib/providers').provideLinter().lint
 
-beforeEach(() => {
-  waitsForPromise(() => {
-    return Promise.all(['linter-spell'].map(p => atom.packages.activatePackage(p)))
+  beforeEach(() => {
+    waitsForPromise(() => {
+      return atom.packages.activatePackage('linter-spell')
+    })
   })
-})
 
   it('finds a spelling in "foo.txt"', () => {
     waitsForPromise(() => {
       return atom.workspace.open(path.join(__dirname, 'files', 'foo.txt')).then(editor => {
         return lint(editor).then(messages => {
           expect(_.some(messages, (message) => { return message.text.match(/^armour( ->|$)/) })).toBe(true)
-          // expect(_.some(messages, (message) => { return message.text.match(/^travelling(:|$)/) })).toBe(true)
         })
       })
     })
